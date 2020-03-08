705ba67c55c4d2f6689f2b086638ac6237ab92b1
Alvaro <Alvaro@DESKTOP-V2PKLEK>
2020-03-08 15:14:44 -0400

    Hola

---
+++ Proyecto2020-1Aplicada2/Proyecto2020-1Aplicada2/Models/Granja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_2020_1.Models
+{
+    public class Granja
+    {
+        [Key]
+        public int MadreId { get; set; }
+        public string Raza { get; set; }
+        public int Corral { get; set; }
+
+    }
+}
